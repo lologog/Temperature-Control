4adcfad9d9f15adbec18c8949040e983daab345a
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2024-11-30 20:48:41 +0100

    project: add project skeleton

---
+++ Core/Inc/regulator_machine.h
@@ -0,0 +1,21 @@
+/*
+ * regulator_machine.h
+ *
+ *  Created on: Nov 30, 2024
+ *      Author: admin
+ */
+
+#ifndef REGULATOR_MACHINE_H
+#define REGULATOR_MACHINE_H
+
+//here are gonna be all includes
+
+//states of the machine
+typedef enum{
+	INITIAL
+} RegualatorMachineState;
+
+void RegulatorMachine_Init(void); //initial funcion of the machine - need to be used only once
+void RegulatorMachine_Run(void); //state machine function - put in loop function
+
+#endif /* REGULATOR_MACHINE_H */
---
+++ Core/Src/regulator_machine.c
@@ -0,0 +1,21 @@
+/*
+ * regulator_machine.c
+ *
+ *  Created on: Nov 30, 2024
+ *      Author: admin
+ */
+
+#include "regulator_machine.h"
+
+//global state of the machine
+static RegualatorMachineState currentState = INITIAL;
+
+void RegulatorMachine_Init(void)
+{
+
+}
+
+void RegulatorMachine_Run(void)
+{
+
+}
