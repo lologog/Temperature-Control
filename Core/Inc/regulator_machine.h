c93a2c0d7a5769cd202624aedac9d98bddc8f692
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2024-12-03 19:03:55 +0100

    project: add INITIAL STATE of the machine

--- Core/Inc/regulator_machine.h
+++ Core/Inc/regulator_machine.h
@@ -14,12 +14,18 @@
 #include "keyboard.h"
 #include "encoder.h"
 
+#include <stdbool.h>
+
 //states of the machine
 typedef enum{
-	INITIAL
+	INITIAL_STATE,
+	REGULATION_TYPE_STATE
 } RegualatorMachineState;
 
 void RegulatorMachine_Init(void); //initial funcion of the machine - need to be used only once
 void RegulatorMachine_Run(void); //state machine function - put in loop function
+
+//states of the machine
+static void HandleInitialState(void);
 
 #endif /* REGULATOR_MACHINE_H */
--- Core/Src/regulator_machine.c
+++ Core/Src/regulator_machine.c
@@ -8,7 +8,7 @@
 #include "regulator_machine.h"
 
 //global state of the machine
-static RegualatorMachineState currentState = INITIAL;
+static RegualatorMachineState currentState = INITIAL_STATE;
 
 void RegulatorMachine_Init(void)
 {
@@ -17,7 +17,54 @@ void RegulatorMachine_Init(void)
 	Encoder_Init();
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 void RegulatorMachine_Run(void)
 {
-
+	switch (currentState)
+	{
+		case INITIAL_STATE:
+			HandleInitialState();
+			break;
+	}
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+static void HandleInitialState(void)
+{
+	static uint32_t last_toggle_time = 0;
+	static bool show_click_start = true;
+
+	// settings leds on the board
+	HAL_GPIO_WritePin(SETTINGS_LED_GPIO_Port, SETTINGS_LED_Pin, 1);
+	HAL_GPIO_WritePin(REGULATION_LED_GPIO_Port, REGULATION_LED_Pin, 0);
+	HAL_GPIO_WritePin(HEATING_LED_GPIO_Port, HEATING_LED_Pin, 0);
+
+	// first line of text
+	lcd_put_cur(0, 2);
+	lcd_send_string("TEMP CONTROL");
+	lcd_put_cur(1, 2);
+
+	//blinking of the second line of text
+	if (HAL_GetTick() - last_toggle_time >= 1000)
+	{
+		if (show_click_start)
+		{
+			lcd_send_string("CLICK START");
+		}
+		else
+		{
+			lcd_send_string("                ");
+		}
+
+		last_toggle_time = HAL_GetTick();
+		show_click_start = !show_click_start;
+	}
+
+	//click start to start choosing regulation type
+	if (HAL_GPIO_ReadPin(START_STOP_GPIO_Port, START_STOP_Pin) == 1)
+	{
+		currentState = REGULATION_TYPE_STATE;
+	}
+}
+
+
