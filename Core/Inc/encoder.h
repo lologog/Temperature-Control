74ad9cf4776a789d406c836fefd1115b5ed3ddea
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2024-12-03 18:36:16 +0100

    project: add clicking encoders

--- Core/Inc/encoder.h
+++ Core/Inc/encoder.h
@@ -1,9 +1,17 @@
+/*
+ * encoder.h
+ *
+ *  Created on: Dec 2, 2024
+ *      Author: Krzysztof Tomicki
+ */
+
 #ifndef ENCODER_H
 #define ENCODER_H
 
 #include "main.h"
 
 void Encoder_Init(void);
-void Encoder_GetPositions(uint8_t encoderNumber);
+int16_t Encoder_GetPosition(uint8_t encoderNumber);
+uint8_t Encoder_GetButtonState(uint8_t encoderNumber); // New function to read button states
 
 #endif // ENCODER_H
--- Core/Src/encoder.c
+++ Core/Src/encoder.c
@@ -20,16 +20,31 @@ void Encoder_Init(void)
     HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
 }
 
-// get the position of a specific encoder
+// Get the position of a specific encoder
 int16_t Encoder_GetPosition(uint8_t encoderNumber)
 {
-    switch (encoderNumber){
+    switch (encoderNumber) {
         case 1:
             return (int16_t)__HAL_TIM_GET_COUNTER(&htim1) / 2;
         case 2:
             return (int16_t)__HAL_TIM_GET_COUNTER(&htim2) / 2;
         case 3:
             return (int16_t)__HAL_TIM_GET_COUNTER(&htim3) / 2;
+        default:
+            return 0;
+    }
+}
+
+// Get the button state of a specific encoder
+uint8_t Encoder_GetButtonState(uint8_t encoderNumber)
+{
+    switch (encoderNumber) {
+        case 1:
+            return HAL_GPIO_ReadPin(ENC1_BUTTON_GPIO_Port, ENC1_BUTTON_Pin) == GPIO_PIN_RESET ? 1 : 0;
+        case 2:
+            return HAL_GPIO_ReadPin(ENC2_BUTTON_GPIO_Port, ENC2_BUTTON_Pin) == GPIO_PIN_RESET ? 1 : 0;
+        case 3:
+            return HAL_GPIO_ReadPin(ENC3_BUTTON_GPIO_Port, ENC3_BUTTON_Pin) == GPIO_PIN_RESET ? 1 : 0;
         default:
             return 0;
     }
